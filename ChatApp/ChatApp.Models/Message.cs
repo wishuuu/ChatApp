8985a33b97a3e9ff668aa127545233cdc34777bb
Oskar Wiszowaty <ppwiszu@gmail.com>
2022-07-23 12:04:39 +0200

    Initial commit

---
+++ ChatApp/ChatApp.Models/Message.cs
@@ -0,0 +1,7 @@
+namespace Models;
+
+public class Message
+{
+    public string Sender { get; set; }
+    public string Text { get; set; }
+}
---
+++ ChatApp/ChatApp.SignalRServer/Hubs/UserHub.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.API.Hubs;
+
+public class UserHub : Hub
+{
+    private readonly ILogger<UserHub> _logger;
+
+    public UserHub(ILogger<UserHub> logger)
+    {
+        this._logger = logger;
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        _logger.LogInformation("Connected {ConnectionId}", Context.ConnectionId);
+        
+        return base.OnConnectedAsync();
+    }
+
+    public async Task SendMessage(string message)
+    {
+        await this.Clients.Others.SendAsync("Message", message);
+    }
+
+    public async Task Ping()
+    {
+        await this.Clients.Caller.SendAsync("Ping");
+    }
+}
